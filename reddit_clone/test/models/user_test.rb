246ed2cdf1bfc073e238b923de98de5bb9151096
RyanKim01 <kyhekm@hotmail.com>
2016-02-28 18:23:08 -0800

    Changed regex for email to much harder one, added a test to check whether email.downcase works

--- reddit_clone/test/models/user_test.rb
+++ reddit_clone/test/models/user_test.rb
@@ -54,7 +54,12 @@ class UserTest < ActiveSupport::TestCase
     assert_not duplicate_user.valid?
   end
 
-
+  test "email is always saved in lower-case" do
+    mixed_case_email = "FoO@ExAmPlE.cOm"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
+  end
 
 
 
