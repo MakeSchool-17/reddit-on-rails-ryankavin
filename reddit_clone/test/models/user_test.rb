45e3b3cab94e718bf73ad14c04e3b0c99676c87a
RyanKim01 <kyhekm@hotmail.com>
2016-02-28 17:07:55 -0800

    Added length and presence validation for User model

--- reddit_clone/test/models/user_test.rb
+++ reddit_clone/test/models/user_test.rb
@@ -1,7 +1,32 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(username: "Ryan", email: "example@makeschool.com")
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+  test "username should be present" do
+    @user.username = "   "
+    assert_not @user.valid?
+  end
+
+  test "email should be present" do
+    @user.email = "    "
+    assert_not @user.valid?
+  end
+
+  test "username has a limit in length" do
+    @user.username = "a" * 51
+    assert_not @user.valid?
+  end
+
+  test "email has a limit in length" do
+    @user.email = "a" * 244 + "@example.com"
+    assert_not @user.valid?
+  end
+
 end
