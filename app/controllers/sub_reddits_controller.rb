995982e222e5c12b0607eea63362c97834e5965c
RyanKim01 <kyhekm@hotmail.com>
2016-03-09 20:51:06 -0800

    Added methods for user and subreddit controllers

--- app/controllers/sub_reddits_controller.rb
+++ app/controllers/sub_reddits_controller.rb
@@ -2,13 +2,30 @@ class SubRedditsController < ApplicationController
 
 	def index
 		@sub_reddit = SubReddit.all
-		render json: {status: 200, subreddis: @sub_reddit}
+		render json: {status: 200, subreddits: @sub_reddit}
 		#aaaa
 	end
 
 	def show
 		@sub_reddit = SubReddit.find(params[:id])
-		render json: {status: 200, user: @sub_reddit}
+		render json: {status: 200, subreddit: @sub_reddit}
+	end
+
+	def create
+		@sub_reddit = SubReddit.create(title: params[:title], description: params[:description])
+		render json: {status: 201, created_subreddits: @sub_reddit}
+	end
+
+	def destroy
+		@sub_reddit = SubReddit.find(params[:id])
+		@sub_reddit.destroy
+		render json: {status: 204, destroyed_subreddits: @sub_reddit}
+	end
+
+	def update
+		@sub_reddit = SubReddit.find(params[:id])
+		@sub_reddit.update(title: params[:title], description: params[:description])
+		render json: {status: 201, updated_subreddit: @sub_reddit}
 	end
 
 	def show_by_slug
@@ -21,9 +38,4 @@ class SubRedditsController < ApplicationController
 		end
 	end
 
-	def new
-	end
-
-	def main
-	end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,9 +13,19 @@ class UsersController < ApplicationController
 
   def create
     @user = User.create(username: params[:username], email: params[:email], password: params[:password])
-    render json: {status: 200, user: @user}
+    render json: {status: 201, created_user: @user}
   end
 
+  def update
+    @user = User.find(params[:id])
+    @user.update(username: params[:username], email: params[:email], password: params[:password])
+    render json: {status: 201, updated_user: @user}
+  end
 
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    render json: {status: 204, destroyed_user: @user}
+  end
 
 end
